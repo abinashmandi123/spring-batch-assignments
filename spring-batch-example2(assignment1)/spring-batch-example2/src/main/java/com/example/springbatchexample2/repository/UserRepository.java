d8a34833437c0bfc06ea1f031bd7644933c3413d
abinashmandi123 <84388348+abinashmandi123@users.noreply.github.com>
2021-11-02 10:24:50 +0530

    Delete spring-batch-example2(assignment1)/spring-batch-example2 directory

--- spring-batch-example2(assignment1)/spring-batch-example2/src/main/java/com/example/springbatchexample2/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbatchexample2.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.springbatchexample2.model.User;
-
-public interface UserRepository extends MongoRepository<User,Integer>{
-
-}
